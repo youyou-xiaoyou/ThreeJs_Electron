b146deb0aa331a59b42f03dddbe1056d7edbb8d6
youyou-xiaoyou <2576691011@qq.com>
2023-08-13 19:56:51 +0800

    dev1.1版本完成

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
-      width: 800,
-      height: 600
+      width: 1000,
+      height: 700
     })
   
-    win.loadFile('./project/createRect.html')
+    win.loadFile('./project/view.html')
   }
 app.whenReady().then(() => {
     createWindow()
